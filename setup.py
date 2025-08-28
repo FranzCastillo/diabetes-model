f74414d423018dd1c0cd996ebee36df995b3a0bc
Francisco Castillo <castillocerna@gmail.com>
2025-08-27 19:40:56 -0600

    Added Packages Files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="diabetes-model",
+    version="0.0.2",
+    description="A pipeline for diabetes prediction using logistic regression.",
+    author="Your Name",
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "pandas",
+        "joblib"
+    ],
+    python_requires=">=3.7",
+)
