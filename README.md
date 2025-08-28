955ad5136634204b9b424f7478f70c358b8815e0
Francisco Castillo <castillocerna@gmail.com>
2025-08-27 19:47:33 -0600

    Bump version to 0.0.5

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CI/CD Test
 Tutorial from: https://www.youtube.com/watch?v=NMQwzI9hprg
+
+git add .
+git commit -m "Bump version to 0.0.5"
+git tag 0.0.3
+git push origin main --tags
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="diabetes-model",
-    version="0.0.4",
+    version="0.0.5",
     description="A pipeline for diabetes prediction using logistic regression.",
     author="Your Name",
     packages=find_packages(),
